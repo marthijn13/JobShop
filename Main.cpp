61825557d38ddd2e68af55b5cabb9fc298a41e95
marthijn13 <marthijnvandeelen@gmail.com>
2018-09-18 12:42:29 +0200

    Test push

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,10 @@
  *  Created on: Sep 14, 2018
  *      Author: marthijn
  */
+#include <iostream>
 
+int main(int argc, char **argv) {
+	std::cout << "Hello world!" << std::endl;
 
-
-
+	return 0;
+}
